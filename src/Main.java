109ebc16a5048f8b3e8b66efe271c4cbea4754e6
João Monge <joao.monge13@gmail.com>
2022-11-16 14:37:06 +0000

    Exercicio 2

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        Main.tabuadaExtra();
+        Main.tabuadaIntervalo();
     }
 
     /*
+    Ex1.
     * public - Acesso em qualquer parte do programa
     * static - O método pertence à classe e não às instâncias dela
     * void   - Não têm retorno
@@ -25,6 +25,10 @@ public class Main {
 
     }
 
+    /**
+     * Ex. Extra - Feito em aula não esta na ficha.
+     * Imprimir as tabuadas todas do 1 ao 10
+     */
     public static void tabuadaExtra() {
         for(int j = 1; j < 11; j++) {
             for(int i = 1; i < 11; i++) {
@@ -33,4 +37,24 @@ public class Main {
         }
     }
 
+    /**
+     * Ex2
+     */
+    public static void tabuadaIntervalo(){
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Escreva um número:");
+        int num = s.nextInt();
+
+        System.out.println("Escreva o minimo do intervalo:");
+        int minimo = s.nextInt();
+        System.out.println("Escreva o máximo do intervalo:");
+        int maximo = s.nextInt();
+
+
+        for(int i = minimo; i <= maximo; i++){
+            System.out.println( num + " x " + i + " = " + (i*num));
+        }
+
+    }
 }
